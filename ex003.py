a59f63bbaf2477dc9583114b12e5768950002d64
Rodrigo Lins <altac.rlins@gmail.com>
2017-11-06 17:01:25 -0200

    Delete ex003.py

--- ex003.py
+++
@@ -1,2 +0,0 @@
-# Desafio 003 - Crie um programa que leia dois números e mostre a soma entre eles.
-print('A soma é {}.'.format((int(input('Digite um número: ')))+(int(input('Digite outro número: ')))))
